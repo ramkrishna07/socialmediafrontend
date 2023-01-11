c188f72006c4eba0606ad3c070f1bea7b45884c7
Ramkrishna Saha <rajsahaking456@gmail.com>
2023-01-11 19:05:38 +0530

    latest server

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({baseURL: "https://socialmediabackend-fb91.onrender.com"})
+const API = axios.create({baseURL: "https://socialmediabackend-ud7p.onrender.com"})
 // const API = axios.create({baseURL: "http://localhost:5000"})
 
 export const logIn=(formData)=>API.post('/auth/login',formData)
